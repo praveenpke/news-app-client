314dc1afee2a9a168d4660066a8ffa9c2cadc26e
praveen <35184782+praveenpke@users.noreply.github.com>
2025-01-11 21:03:07 -0600

    android version updates

--- android/app/src/main/java/com/pke/newsapp/MainActivity.java
+++ android/app/src/main/java/com/pke/newsapp/MainActivity.java
@@ -1,5 +1,20 @@
 package com.pke.newsapp;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Access the WebView and clear its cache
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+            webView.clearCache(true);
+        }
+    }
+}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.pke.newsapp',
   appName: 'news-app-client',
   webDir: 'dist/news-app-client/browser',
-  bundledWebRuntime: false,
   server: {
     url: 'https://news-app-client-black.vercel.app/', // Replace with your Vercel URL
     cleartext: true,
